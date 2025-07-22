4a0c82bdde640e719d891380f9de874e29a4f683
Sushant Shrestha <sushantshrestha184@gmail.com>
2025-07-22 17:24:04 +0545

    changes made

--- exampractise/chapter 5/SpecificHeader.java
+++ exampractise/chapter 5/SpecificHeader.java
@@ -1,5 +1,17 @@
 
+import java.net.*;
 
 public class SpecificHeader {
-    
+    public static void main(String[] args) throws Exception{
+        URL url = new URL("http://www.example.com");
+
+        URLConnection con = url.openConnection();
+
+        System.out.println(con.getContentType());
+        System.out.println(con.getContentLength());
+        System.out.println(con.getContentEncoding());
+        System.out.println(con.getDate());
+        System.out.println(con.getExpiration());
+        System.out.println(con.getLastModified());
+    }    
 }
